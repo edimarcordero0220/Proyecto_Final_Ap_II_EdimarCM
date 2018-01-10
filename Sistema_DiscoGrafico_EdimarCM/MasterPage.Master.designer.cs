c7003b706e12ef992bc8ef3878faa4de83094250
Edimarcordero0220 <edicordero98@gmail.com>
2018-01-10 22:08:02 +0300

    Cojiendo forma!

--- Sistema_DiscoGrafico_EdimarCM/MasterPage.Master.designer.cs
+++ Sistema_DiscoGrafico_EdimarCM/MasterPage.Master.designer.cs
@@ -22,13 +22,13 @@ namespace Sistema_DiscoGrafico_EdimarCM {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// form1 control.
+        /// formMaster control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm formMaster;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
